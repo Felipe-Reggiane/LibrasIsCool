ec9e87aa1a1982ac5591ea1f5ffaa49fbd131878
Felipe <felipe.reggiane@rethink.dev>
2023-12-06 22:15:17 -0300

    feat: tutorial modal

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,8 @@ import chatIcon from "./chatIcon.svg";
 import userIcon from "./Ellipse 1.svg";
 import logoutIcon from "./ic_outline-logout.svg";
 import arrow from "./arrow.svg";
+import tutorialIcon from "./tutorialIcon.svg";
+import closeModalIcon from "./closeModalIcon.svg";
 
 const images = {
   gatinho,
@@ -10,6 +12,8 @@ const images = {
   userIcon,
   logoutIcon,
   arrow,
+  tutorialIcon,
+  closeModalIcon,
 };
 
 export default images;
--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,12 +1,25 @@
 import VLibras from "@djpfs/react-vlibras";
+import images from "../../assets";
 import Sidebar from "../sidebar/sidebar";
 import SlideButton from "../slideButton/slideButton";
+import { useState } from "react";
 
 import styles from "./layout.module.css";
+// import Tutorial from "../tutorial";
+import TutorialModal from "../tutorial";
 
 const Layout = ({ children }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <div className={styles.layoutContainer}>
+      <img
+        src={images.tutorialIcon}
+        alt="tutorial"
+        className={styles.tutorial}
+        onClick={() => setModalOpen(true)}
+      />
+      <TutorialModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
       <VLibras forceOnload={true} />
       <Sidebar />
       <div className={styles.childrenContainer}>
